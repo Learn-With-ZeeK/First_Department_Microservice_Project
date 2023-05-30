a4061e7d9038b54261dd99fd391596b21e478c81
Zeeshan A Khan <104137451+Learn-With-ZeeK@users.noreply.github.com>
2023-05-30 18:57:50 +0530

    Main file of the project

--- department-service-A/src/main/java/com/zeek/departmentserviceA/DepartmentServiceAApplication.java
+++ department-service-A/src/main/java/com/zeek/departmentserviceA/DepartmentServiceAApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DepartmentServiceAApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DepartmentServiceAApplication.class, args);
+	    SpringApplication.run(DepartmentServiceAApplication.class, args);
 	}
 
 }
