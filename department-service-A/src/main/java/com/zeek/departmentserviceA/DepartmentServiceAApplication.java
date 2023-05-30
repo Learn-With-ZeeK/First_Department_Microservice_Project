53a90984ce08417c846d696dc3ff48398706cd1a
Zeeshan Khan <learnwithzeek@gmail.com>
2023-05-30 18:46:55 +0530

    First Microservice department linked with user

---
+++ department-service-A/src/main/java/com/zeek/departmentserviceA/DepartmentServiceAApplication.java
@@ -0,0 +1,13 @@
+package com.zeek.departmentserviceA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentServiceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentServiceAApplication.class, args);
+	}
+
+}
