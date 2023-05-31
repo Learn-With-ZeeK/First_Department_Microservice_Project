c63995c69705e0c09f232415f3ed8b361dfb8ec0
Zeeshan A Khan <104137451+Learn-With-ZeeK@users.noreply.github.com>
2023-05-31 15:58:31 +0530

    Repository Class

--- department-service-A/src/main/java/com/zeek/departmentserviceA/repository/DepartmentRepository.java
+++ department-service-A/src/main/java/com/zeek/departmentserviceA/repository/DepartmentRepository.java
@@ -1,11 +1,8 @@
 package com.zeek.departmentserviceA.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.zeek.departmentserviceA.entity.Department;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
-
-  Department findByDepartmentId(Long departmentId);
-  
-}
+ Department findByDepartmentId(Long departmentId);
+  }
